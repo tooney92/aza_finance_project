83d0158e29de4fbd549e3949e9bc830df0478e6b
capn-jack-sparrow <tooneynta92@gmail.com>
2022-10-03 22:00:03 +0100

    adds active record numericality constraint.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,8 +1,8 @@
 class Transaction < ApplicationRecord
   validates_presence_of :customer_id
   validates_presence_of :input_amount
-  validates_numericality_of :input_amount
+  validates_numericality_of :input_amount, greater_than: 0
   validates_presence_of :input_currency
-  validates_numericality_of :output_amount
+  validates_numericality_of :output_amount, greater_than: 0
   validates_presence_of :output_currency
 end
